a23d3c2718f2167bbb72e07ea0ed58d3f94ef376
kboske <kboske@comcast.net>
2017-02-17 22:26:42 -0500

    Update FruitServer.java

--- restful-no-seeds/src/test/java/modules/FruitServer.java
+++ restful-no-seeds/src/test/java/modules/FruitServer.java
@@ -1,45 +1,27 @@
 package modules;
 
 import java.util.EnumSet;
-
 import javax.servlet.DispatcherType;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-import com.sun.jersey.spi.container.servlet.ServletContainer;
-
 
 public class FruitServer {
 	private final Server server;
 	
 	public FruitServer(int port){
-		ServletHolder sh = new ServletHolder(
-				ServletContainer.class);
-		sh.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", 
-				"com.sun.jersey.api.core.PackagesResourceConfig");
-		sh.setInitParameter("com.sun.jersey.config.property.packages", "modules");
-		sh.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
-		sh.setInitParameter("mimeTypes", "application/json");
-		sh.setInitParameter("allowedOrigins", "*");
-	    sh.setInitParameter("allowedMethods", "GET");
-	    
-		server = new Server(port);
 		
 		FilterHolder filterHolder = new FilterHolder();
-	    filterHolder.setInitParameter("allowedOrigins", "*");
-	    filterHolder.setInitParameter("allowedMethods", "GET");
-	    filterHolder.setClassName("com.sun.jersey.spi.container.servlet.ServletContainer");
-	    filterHolder.setInitParameter("com.sun.jersey.config.property.packages", "modules");
-	    filterHolder.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
-	    filterHolder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", 
+	        filterHolder.setInitParameter("allowedOrigins", "*");
+	        filterHolder.setInitParameter("allowedMethods", "GET");
+	        filterHolder.setClassName("com.sun.jersey.spi.container.servlet.ServletContainer");
+	        filterHolder.setInitParameter("com.sun.jersey.config.property.packages", "modules");
+	        filterHolder.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+	        filterHolder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", 
 				"com.sun.jersey.api.core.PackagesResourceConfig");
-	    filterHolder.setInitParameter("mimeTypes", "application/json");
+	        filterHolder.setInitParameter("mimeTypes", "application/json");
 	    
 		ServletContextHandler context = new ServletContextHandler(server,"/",ServletContextHandler.SESSIONS);
-		context.addServlet(sh, "/");
 		context.addFilter(filterHolder, "/*", EnumSet.of(DispatcherType.INCLUDE,DispatcherType.REQUEST));
 		context.setAvailable(true);
 	
